5f6178d874540a0f9564aa85ca23d08ec05048ff
nihardash1981 <contacttonihar@gmail.com>
2023-05-11 17:23:44 -0400

    commit successfully

---
+++ pyhonslicing.py
@@ -0,0 +1,15 @@
+s="python"
+print(s[0:4])
+print(s[-6:-2])
+print(s[1:5])
+print(s[2:4])
+print(s[-4:-2])
+print(s[-5:1])
+print(s[12:1])
+print(s[-6:-2])
+print(s[1:50])
+print(s[-100:-2])
+print(s[1:])
+print(s[:6])
+print(s[:4])
+    
