5c6cdaf7c11bcf434eac58d71bda9174f0340dfe
nihardash1981 <contacttonihar@gmail.com>
2023-05-17 23:43:53 -0400

    commit list objects

---
+++ pythonlist.py
@@ -0,0 +1,12 @@
+l1=[10,20,30,40,50,60,70,80,90,100,-10,20]
+print(l1,type(l1))
+l2=[10,10.94, "rosan", "aguata",2+3j]
+print(l2,type(l2))
+l3=[]
+print(l3,type(l3))
+print(l2[1])
+print(l2[-1])
+print(l1[1:3]) #slicing the oobjects list
+print(l1[1:-2])
+
+
