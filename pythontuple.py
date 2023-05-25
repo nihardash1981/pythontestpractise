efec1f8315f53b7329db16f646c476d91b6252a7
nihardash1981 <contacttonihar@gmail.com>
2023-05-25 15:38:26 -0400

    commit tuple to list and list to tuple

--- pythontuple.py
+++ pythontuple.py
@@ -10,4 +10,11 @@ t4=10,"travis","javis",67.98,76.98,"ouacet",2+3j
 print(t4,type(t4)) ###class tuple without braces also tuple###
 print(t4[0]) ###class tuple inndex value###
 print(t4[-1]) ###class tuple index value
-
+print(t4[::-1]) ###class tuple with slicing###
+###convert list to tuple###
+lst=[10,20,30,40,50,"python"]
+print(lst,type(lst)) ###class
+tpl=tuple(lst)
+print(tpl,type(tpl)) ##class 'tuple convert list to tuple###
+lst1=list(tpl)
+print(lst1,type(lst1)) ###convert tuple to list###
