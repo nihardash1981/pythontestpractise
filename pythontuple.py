5a1f4cc28efe7fa50711f00dbb49f516bcd61c4d
nihardash1981 <contacttonihar@gmail.com>
2023-05-25 00:59:17 -0400

    commited tuples

---
+++ pythontuple.py
@@ -0,0 +1,13 @@
+t1=(10,20,30,40,76,32,98)
+print(t1,type(t1)) ###class tuple##
+t2=(10,20,"kvr","naresh","python",32,9,12,7,True)
+print(t2,type(t2)) ###class tuple accept both homogenous and hetrogenous data##
+t3=()
+print(t3,type(t3)) ###class tuple##
+t3=tuple()
+print(t3,type(t3)) ###class tuple###
+t4=10,"travis","javis",67.98,76.98,"ouacet",2+3j
+print(t4,type(t4)) ###class tuple without braces also tuple###
+print(t4[0]) ###class tuple inndex value###
+print(t4[-1]) ###class tuple index value
+
