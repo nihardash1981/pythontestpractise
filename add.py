e8b090e243c70645547f52e0e2c01efac6b1b960
nihardash1981 <contacttonihar@gmail.com>
2023-06-01 00:57:16 -0400

    commit add function

---
+++ add.py
@@ -0,0 +1,16 @@
+##multiplication of two numbers##
+print("enter first number:")
+a=input()
+print("enter second number:")
+b=input()
+###c=a*b ##cant multiply 2str 2non integer##
+x1=int(a)
+x2=int(b)
+x3=x1*x2
+print("val of x1=",x1)
+print("val of x2=",x2)
+print("mul=",x3)
+
+
+
+
