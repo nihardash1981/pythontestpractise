c3077dc8b84e0dbe4554a3753c7738c3fe397338
nihardash1981 <contacttonihar@gmail.com>
2023-06-04 16:56:54 -0400

    commit float value

---
+++ inputmsgaddmul.py
@@ -0,0 +1,13 @@
+a=float(input("enter the  value:")) ##: add to get spcae##
+b=float(input("enter the value:"))
+c=a*b
+print("-"*40)
+print("result")
+print("-"*40)
+print("val of a=",a)
+print("val of b=",b)
+print("val of c=",c)
+
+
+
+
