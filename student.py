47e3808fbc252c4ef0d4f7da63a3b8ec92f0d745
nihardash1981 <contacttonihar@gmail.com>
2023-06-05 00:19:29 -0400

    stuednt example

---
+++ student.py
@@ -0,0 +1,23 @@
+sno=int(input("enter the students number:"))
+sname=input("Enter the student name:")
+marks=float(input("Enter the students marks:"))
+cname=input("Enter the student college name")
+
+#display the students details##
+print("-"*50)
+print("Enter the students details")
+print("-"*50)
+print("student number :{}".format(sno))
+print("student marks :{}".format(sname))
+print("student cname :{}".format(marks))
+print("student college name :{}".format(cname))
+print("-"*50) ##50times the - displayed##
+
+##tempreture formula##
+# ft=(9/5)*ct+32
+#0R#
+#ft=1.8*ct+32###
+##here ct as int type float type##
+
+
+
