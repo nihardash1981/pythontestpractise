c7fb9a83237cdc470fa215fe4fb69147a120415b
nihardash1981 <contacttonihar@gmail.com>
2023-06-05 01:26:51 -0400

    operators description

---
+++ operators.py
@@ -0,0 +1,14 @@
+a=10
+b=20
+c=a*b
+print(a,b,c)
+print("chn"*3) #repetation operator##
+print(10/3)##float quotient##
+print(10//3)##integer quotient##
+print(10/2)##flaot  quotient##
+n=81
+n**0.5
+print(n)
+import math
+math.sqrt(n)
+print(math.sqrt(n))
