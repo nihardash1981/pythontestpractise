acea18cb0b4513e4c44c34406a6bf39e14d488d3
nihardash1981 <contacttonihar@gmail.com>
2023-06-04 23:08:15 -0400

    print types declarations

---
+++ printtypes.py
@@ -0,0 +1,15 @@
+a=10
+print(a)
+print("val of a=",a)
+print("{}",format(a)) # print flower bracket space a value if put .if put,flower and 10 print##
+a,b=10,20
+print("{} {}".format(a,b))
+print("val of a={}".format(a))
+print("val of a={} and value of b={}".format(a,b))  # print flower brackt and b value##
+
+##calculation##
+a,b=10,20
+c=a+b
+print("sum of {} and {}={}".format(a,b,c))
+
+
