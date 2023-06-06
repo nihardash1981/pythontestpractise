9009f14579347c1b718cbf6a9cdd6747fd50c61c
nihardash1981 <contacttonihar@gmail.com>
2023-06-06 13:23:13 -0400

    commited swapping

---
+++ swap.py
@@ -0,0 +1,12 @@
+a=input("Enter the first value:")
+b=input("Enter the second value:")
+print("-"*50)
+print("original val of a={}".format(a))
+print("original val of b={}".format(b))
+print("-"*50)
+a,b=b,a #multiline assignment##
+print("swapped val of a={}".format(a)) #swapped both integer and string values
+print("swapped val of b={}".format(b))
+print("-"*50)
+
+
