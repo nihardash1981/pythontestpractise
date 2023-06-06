db5d438f7d85769c76071878f3e7ddbe9a4d3c2d
nihardash1981 <contacttonihar@gmail.com>
2023-06-06 12:23:34 -0400

    commit arithmetic test

---
+++ arithimetic.py
@@ -0,0 +1,13 @@
+a=int(input("Enter the value of a:"))
+b=int(input("Enter the value of b:"))
+##calculation##
+print("-"*40) #40times  space##
+print("{} + {}={}".format(a,b,a+b))
+print("{} - {}={}".format(a,b,a-b))
+print("{} * {}={}".format(a,b,a*b))
+print("{} / {}={}".format(a,b,a/b))##get with float value##
+print("{} // {}={}".format(a,b,a//b))##integer chotent value in output##
+print("{} % {}={}".format(a,b,a%b))##only reminder in output##
+print("{} ** {}={}".format(a,b,a**b))##a to the power b##
+print("-"*40)
+
