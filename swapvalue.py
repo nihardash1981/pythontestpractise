c26bd63694a5d556ec7deb399380dfc952522855
nihardash1981 <contacttonihar@gmail.com>
2023-06-08 20:02:43 -0400

    commited

---
+++ swapvalue.py
@@ -0,0 +1,11 @@
+x=int(input("Enter valof x:"))
+y=int(input("Enter valof y:"))
+print("-"*50)
+print("original value of x={}".format(x))
+print("original value of y={}".format(y))
+print("-"*50)
+#swapping logic##
+x,y=y,x
+print("swapped value of x={}".format(x))
+print("swapped value of y={}".format(y))
+print("-"*50)
