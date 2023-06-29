7251005f584918ed3a707d838a2ab76cdb5942dc
nihardash1981 <contacttonihar@gmail.com>
2023-06-29 13:17:37 -0400

    committed

---
+++ digitsif.py
@@ -0,0 +1,23 @@
+d=int(input("Enter any digit:"))
+if(d==0):
+    print("Zero")
+elif(d==1):
+    print("One")
+elif(d==2):
+    print("Two")
+elif(d==4):
+    print("Three")
+elif(d==5):
+    print("Four")
+elif(d==6):
+    print("Five")
+elif(d==7):
+    print("Six")
+elif(d==8):
+    print("Seven")
+else:
+    print("its not a digit")
+    print("\nim other part program")
+            
+
+          
---
+++ numbergen.py
@@ -0,0 +1,17 @@
+n=int(input("ENTER a number:"))
+if(n<=0):
+     print("{} is invalid input:".format(n))
+else:
+     print("-"*50)
+     print("numbers within :{}".format(n))
+     print("-"*50)
+i=1
+while(i<=n):
+     print("\t{}".format(i))
+     i=i+1
+else:
+     print("-"*50)
+     
+
+
+            
